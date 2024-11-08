992bc01ecc3490255057e81d9cd6b3d0736c70b6
Lucas Dias Leandro <147958304+lucasserdev@users.noreply.github.com>
2024-11-08 15:58:40 -0300

    att

--- login.php
+++ login.php
@@ -2,16 +2,30 @@
 session_start();
 
 ?>
-<h1>Pagina de login</h1>
-<form action="login_action.php" method="post">
-    <label for="">
-        Usuario: <br>
-        <input type="text" name="usuario" id="">   
-    </label> <br> <br>
-    <label for="">
-        Senha: <br>
-        <input type="password" name="pass" id="">
-    </label> <br> <br>
 
-    <input type="submit" value="Entrar">
-</form>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página de Login</title>
+    <link rel="stylesheet" href="assets/css/login.css">
+</head>
+<body>
+
+    <div class="container_form">
+        <h3>Login</h3>
+        <form action="login_action.php" method="post">
+            <label for="">
+                Usuario: <br>
+                <input type="text" name="usuario" id="">
+            </label> <br> <br>
+            <label for="">
+                Senha: <br>
+                <input type="password" name="pass" id="">
+            </label> <br> <br>
+            <input type="submit" value="Entrar">
+        </form>
+    </div>
+</body>
+</html>
